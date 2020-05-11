e0d12c6e908ff4aaebb29e63b533dd1d83c4a608
Kevin Gurton <kevin.gurton@gmail.com>
2020-05-11 15:16:09 +0100

    Changed check to greater than or equal to

--- module2/budget_2.go
+++ module2/budget_2.go
@@ -78,7 +78,7 @@ func (b *Budget) RemoveItem(description string) {
 
 // CreateBudget creates a new budget with a specified max
 func CreateBudget(month time.Month, max float32) (*Budget, error) {
-	if len(report) == 12 {
+	if len(report) >= 12 {
 		return nil, errReportIsFull
 	}
 
