7b36e6cf246bdeb1fda60ee5475769e70b658fcd
Kevin Gurton <kevin.gurton@gmail.com>
2020-05-11 15:05:24 +0100

    Implemented AddItem

--- module2/budget_2.go
+++ module2/budget_2.go
@@ -53,6 +53,11 @@ var errDuplicateEntry = errors.New("Cannot add duplicate entry")
 
 // AddItem adds an item to the current budget
 func (b *Budget) AddItem(description string, price float32) error {
+	newItem := Item{
+		Description: description,
+		Price:       price,
+	}
+	b.Items = append(b.Items, newItem)
 
 	return nil
 }
